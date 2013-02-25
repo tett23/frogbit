3189aff9447a969f99d1d6a017b74ac528fa727f
tett23 <tett23@gmail.com>
2013-02-26 04:43:08 +0900

    Video情報格納時にEncodeQueueも設定するようにした

--- models/encode_queue.rb
+++ models/encode_queue.rb
@@ -7,5 +7,24 @@ class EncodeQueue
   property :priority, Integer, :default=>100
   property :created_at, DateTime
 
-  belongs_to :video, :required=>false
+  belongs_to :video, :unique=>true
+
+  def self.add_last(video_id)
+    encode_queue = EncodeQueue.get(:video_id=>video_id)
+    return encode_queue unless encode_queue.nil?
+
+    self.create({
+      priority: self.last_priority(),
+      video_id: video_id
+    })
+  end
+
+  def self.last_priority
+    encode_queue = self.all(:order=>:priority.desc).first
+    p encode_queue
+
+    return 1 if encode_queue.nil?
+
+    encode_queue.priority+1
+  end
 end
