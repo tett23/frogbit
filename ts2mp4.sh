71d440385116a7560df9dda8633542c085d381fc
tett23 <tett23@gmail.com>
2013-02-26 09:51:12 +0900

    エンコードの高画質化

--- ts2mp4.sh
+++ ts2mp4.sh
@@ -8,8 +8,8 @@ BASE=$(basename $TS .ts)
 #CPU_CORES=0
 CPU_CORES=$(/usr/bin/getconf _NPROCESSORS_ONLN)
 
-X264_HIGH_HDTV="-f mp4 -vcodec libx264 \
-  -fpre preset/libx264-hq-ts.ffpreset \
+X264_HIGH_HDTV="-f mp4 -vf yadif=0 -vcodec libx264 x264opts \
+  -fpre preset/libx264-hq-ts_3.ffpreset \
   -r 30000/1001 -aspect 16:9 -s 1280x720 -bufsize 20000k -maxrate 25000k \
   -acodec libfaac -ac 2 -ar 48000 -ab 128k -threads ${CPU_CORES}"
 
