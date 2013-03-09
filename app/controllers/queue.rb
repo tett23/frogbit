215932655e13c239339d2f5296058e7167058203
tett23 <tett23@gmail.com>
2013-03-10 06:29:55 +0900

    エンコードはいったん手元に書き出してからNASに格納する

--- app/controllers/queue.rb
+++ app/controllers/queue.rb
@@ -79,8 +79,11 @@ Frogbit.controllers :queue do
     end
     EM.defer do
       result = encode_queue.encode()
-      encode_queue.video.update(:is_encoded=>true, :encode_log=>result[:log], :saved_directory=>$config[:output_dir])
-      encode_queue.destroy
+
+      if result.result
+        encode_queue.video.update(:is_encoded=>true, :encode_log=>result[:log], :saved_directory=>$config[:output_dir])
+        encode_queue.destroy
+      end
     end
 
     flash[:info] = "「#{encode_queue.video.output_name}」のエンコードを開始しました"
