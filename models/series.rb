de7c36b652e01d76c947984b35d896550cdeaf6d
tett23 <tett23@gmail.com>
2013-04-08 10:03:43 +0900

    シリーズ名の表示

--- models/series.rb
+++ models/series.rb
@@ -5,5 +5,19 @@ class Series
 
   property :id, Serial
   property :name, String
-  property :period, Integer
+  property :alt_name, String
+  property :period, Integer, :default=>1
+  property :created_at, DateTime
+  property :updated_at, DateTime
+
+  has n, :videos
+
+  def self.list(options={})
+    default = {
+      order: [:name, :period, :id.desc]
+    }
+    options = default.merge(options)
+
+    all(options)
+  end
 end
