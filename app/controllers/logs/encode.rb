4e899ff5ffde40f2c42932bfc7bf4d1fa3094d6c
tett23 <tett23@gmail.com>
2013-05-17 16:11:27 +0900

    TS未削除のみのログを表示するページを作成

--- app/controllers/logs/encode.rb
+++ app/controllers/logs/encode.rb
@@ -3,6 +3,15 @@
 Frogbit.controllers :encode_logs, map: '/logs' do
   get :index do
     @logs = EncodeLog.list()
+    if !params[:exist_ts].nil? && params[:exist_ts].to_boolean
+      @logs = @logs.map do |encode_log|
+        next nil if encode_log.video.nil?
+        next nil unless encode_log.video.exists_ts?
+
+        encode_log
+      end.compact
+    end
+
 
     render 'logs/encode/index'
   end
