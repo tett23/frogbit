a64a5dd9f9ca48ce60803692eaa84314d44adb9a
tett23 <tett23@gmail.com>
2014-03-17 23:52:05 +0900

    TS未削除ページを前コミットと差し替え＋不要になった処理の削除

--- app/controllers/logs/encode.rb
+++ app/controllers/logs/encode.rb
@@ -3,16 +3,6 @@
 Frogbit.controllers :encode_logs, map: '/logs' do
   get :index do
     @logs = EncodeLog.list().page(params[:page] || 1)
-    if !params[:exist_ts].nil? && params[:exist_ts].to_boolean
-      @logs = @logs.map do |encode_log|
-        next nil if encode_log.video.nil?
-        next nil unless encode_log.video.exists_ts?
-
-        encode_log
-      end.compact
-
-      @logs = Kaminari.paginate_array(@logs).page # ページネート可能な配列に変換
-    end
 
     render 'logs/encode/index'
   end
