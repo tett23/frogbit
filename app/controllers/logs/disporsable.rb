a17411f75c016b8280d613494926e9c74cf10594
tett23 <tett23@gmail.com>
2014-04-02 16:52:16 +0900

    TS未削除ページが上手く動いてなかった

--- app/controllers/logs/disporsable.rb
+++ app/controllers/logs/disporsable.rb
@@ -4,7 +4,10 @@ Frogbit.controllers :disporsable, map: '/logs' do
   get :index do
     videos = Video.disporsable(params[:size])
 
-    @logs = EncodeLog.list(video: videos).page(params[:page] || 1)
+    video_ids = videos.map do |v|
+      v.id
+    end
+    @logs = EncodeLog.list(video_id: video_ids).page(params[:page] || 1)
     @logs = Kaminari.paginate_array(@logs).page # ページネート可能な配列に変換
 
     render 'logs/encode/index'
@@ -12,7 +15,11 @@ Frogbit.controllers :disporsable, map: '/logs' do
 
   delete :destroy, provides: :json do
     result = Video.disporsable(params[:size]).map do |video|
-      video.destroy_ts
+      begin
+        video.destroy_ts
+      rescue
+        p $!
+      end
     end
 
     (result.size.zero? || (result.uniq.size == 1 && result.first == true)).to_json
